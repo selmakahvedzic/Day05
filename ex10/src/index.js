f86ae65743a80daa960238f7e97e28dc05d4fba9
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 23:19:15 +0100

    ex10 done !

---
+++ ex10/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+  //Only change code below this line
+  var myString = "Jello World";
+  myString = "Hello World";
+  //only change code abow this line
+
+  return myString;
+}
+
+console.log(main());
+module.exports = main;

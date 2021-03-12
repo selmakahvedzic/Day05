1a9e43903ccee1fdfe7829d8fb00fb2736900690
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 17:02:03 +0100

    ex05 done ! Concate +=

---
+++ ex05/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this line
+     var myString = "Concatenation is the process of appending ";
+     myString += "one string to the end of another string.";
+    //only change code abow this line
+
+    return myString;
+}
+
+console.log(main()); 
+module.exports = main;

be70d91397d7ce9e4f88518c6ee5bc8e9f3d984e
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 22:53:38 +0100

    ex07 done ! String += variable

---
+++ ex07/src/index.js
@@ -0,0 +1,12 @@
+function main(){
+    //Only change code below this line
+     var someAdjective = "the best!"; 
+     var myString = "Mistral is ";
+     myString+=someAdjective;
+    //only change code abow this line
+
+    return myString;
+}
+
+console.log(main()); 
+module.exports = main;

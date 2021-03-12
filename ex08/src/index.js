70a234625e03e56bc9e2de417502986afb0de22d
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 23:03:23 +0100

    ex08 done ! Length of a string

---
+++ ex08/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+  //Only change code below this line
+  var lastName = "Doe";
+  var lastNameLength;
+  lastNameLength = lastName.length;
+  //only change code abow this line
+
+  return lastNameLength;
+}
+
+console.log(main());
+module.exports = main;

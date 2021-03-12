7c2ca555b03ef1e24f4c6558102599663386791c
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 22:49:16 +0100

    ex06 done ! Strings + variables

---
+++ ex06/src/index.js
@@ -0,0 +1,11 @@
+function main(){
+    //Only change code below this line
+     var myName = "Selma"; 
+     var myString = "My name is " + myName + " and I am well!";
+    //only change code abow this line
+
+    return myString;
+}
+
+console.log(main()); 
+module.exports = main;

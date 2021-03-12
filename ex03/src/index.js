732b2b85ddc4414232909b0556001120f69496b4
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 16:53:09 +0100

    ex03 done ! Escape sequences

---
+++ ex03/src/index.js
@@ -0,0 +1,10 @@
+function main(){
+    //Only change code below this line
+     var myString = "FirstLine\n\tSecondLine\nThirdLine";
+    //only change code abow this line
+
+    return myString;
+}
+
+console.log(main()); 
+module.exports = main;

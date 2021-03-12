83538bd27e7294d495e54936e29219b267f10dc2
selmakahvedzic <selma.kahvedzic97@gmail.com>
2021-03-12 23:23:00 +0100

    ex11 done !

---
+++ ex11/src/index.js
@@ -0,0 +1,11 @@
+function main() {
+    //Only change code below this line
+    var lastName = "Tesla";
+    var thirdLetterOfLastName = lastName[2];
+  
+    return thirdLetterOfLastName;
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
